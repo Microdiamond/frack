988d3f9e6c483e1ed8b1706677bc781fdcc5309b
Microdiamond <chatchai.nak1997@gmail.com>
2025-04-24 14:27:22 +0700

    first commit

---
+++ multi-rack/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3030;
+
+// Serve static files
+app.use(express.static('public'));
+
+// Routes for different racks
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/rack1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'rack1.html'));
+});
+
+app.get('/racka4', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'racka4.html'));
+});
+
+app.get('/racka5', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'racka5.html'));
+});
+
+// API endpoints for rack data
+app.get('/api/rack1/devices', (req, res) => {
+    res.json(require('./data/rack1.json'));
+});
+
+app.get('/api/racka4/devices', (req, res) => {
+    res.json(require('./data/racka4.json'));
+});
+
+app.get('/api/racka5/devices', (req, res) => {
+    res.json(require('./data/racka5.json'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
