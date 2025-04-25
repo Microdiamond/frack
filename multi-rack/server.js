7cb7000008bf650ac343cbb78fffaa8c0093f2f7
Microdiamond <chatchai.nak1997@gmail.com>
2025-04-25 15:02:37 +0700

    rack A6

--- multi-rack/server.js
+++ multi-rack/server.js
@@ -23,6 +23,10 @@ app.get('/racka5', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'racka5.html'));
 });
 
+app.get('/racka6', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'racka6.html'));
+});
+
 // API endpoints for rack data
 app.get('/api/rack1/devices', (req, res) => {
     res.json(require('./data/rack1.json'));
@@ -34,6 +38,10 @@ app.get('/api/racka4/devices', (req, res) => {
 
 app.get('/api/racka5/devices', (req, res) => {
     res.json(require('./data/racka5.json'));
+});
+
+app.get('/api/racka6/devices', (req, res) => {
+    res.json(require('./data/racka6.json'));
 });
 
 app.listen(port, () => {
