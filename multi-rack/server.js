78b532e9fc0b9451c27e221d2b6c3610984ca985
Microdiamond <chatchai.nak1997@gmail.com>
2025-04-28 14:25:43 +0700

    feat: create rack b1

--- multi-rack/server.js
+++ multi-rack/server.js
@@ -35,6 +35,10 @@ app.get('/racka8', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'racka8.html'));
 });
 
+app.get('/rackb1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'rackb1.html'));
+});
+
 // API endpoints for rack data
 app.get('/api/rack1/devices', (req, res) => {
     res.json(require('./data/rack1.json'));
@@ -58,6 +62,10 @@ app.get('/api/racka7/devices', (req, res) => {
 
 app.get('/api/racka8/devices', (req, res) => {
     res.json(require('./data/racka8.json'));
+});
+
+app.get('/api/rackb1/devices', (req, res) => {
+    res.json(require('./data/rackb1.json'));
 });
 
 app.listen(port, () => {
